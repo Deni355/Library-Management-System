3b62e159e5e12084a5124be2e60de6457fd3bf6f
Deni355 <dkardjaliev@gmail.com>
2025-09-20 19:00:15 +0200

    First commitof feature branch

---
+++ src/BookManager.java
@@ -0,0 +1,6 @@
+public class BookManager {
+    public void AddBook() {}
+    public void BorrowBook() {}
+    public void ReturnBook() {}
+    public void SearchBook() {}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public void Menu() {
+        System.out.println(
+                "Choose an option:\n" +
+                "1. Add Book\n" +
+                "2. Add User\n" +
+                "3. Borrow Book\n" +
+                "4. Return Book\n" +
+                "5. Search Books\n" +
+                "6. Exit");
+
+        // Takes input
+        Scanner reader = new Scanner(System.in);
+        int choice = reader.nextInt();
+        reader.close();
+
+        // Methods for BookManager
+        BookManager manager = new BookManager();
+
+        // Options
+        switch (choice) {
+            case 1:
+                manager.AddBook();
+                break;
+            case 2:
+                AddUser();
+                break;
+            case 3:
+                manager.BorrowBook();
+                break;
+            case 4:
+                manager.ReturnBook();
+                break;
+            case 5:
+                manager.SearchBook();
+                break;
+            case 6:
+                System.exit(0);
+            default:
+                System.out.println("Wrong choice, try again");
+                Menu();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Welcome to Library System!");
+        Menu();
+
+
     }
 }
