14d1183d4be9c06ac2f1e97533e07272d3d9f3d7
Deni355 <dkardjaliev@gmail.com>
2026-01-27 13:08:08 +0200

    New users and relationship tables

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,4 @@
+/*Table for books*/
 CREATE TABLE IF NOT EXISTS books (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
@@ -5,3 +6,20 @@ CREATE TABLE IF NOT EXISTS books (
     year INT,
     available BOOLEAN NOT NULL DEFAULT TRUE
 );
+
+/*Table for users*/
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+/*Relationship for users and books*/
+CREATE TABLE IF NOT EXISTS borrowings (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    book_id INT NOT NULL REFERENCES books(id),
+    borrowed_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    returned_at TIMESTAMP
+);
+
