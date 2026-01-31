4d41c6ea3065ae868b5d9bbec18e5decca1a489b
Deni355 <dkardjaliev@gmail.com>
2026-02-01 00:02:18 +0100

    new login, register and welcome functions

--- src/Main.java
+++ src/Main.java
@@ -2,64 +2,97 @@ import java.sql.Connection;
 import java.util.Scanner;
 
 public class Main {
+    static final Scanner reader = new Scanner(System.in); // The reader for the input
+    static final BookManager manager = new BookManager(); // BookManager
+
+    static void welcome() {
+        while (true) {
+            System.out.println(
+                    "Welcome to Library System!\n" +
+                    "Choose an option:\n" +
+                    "1. Login\n" +
+                    "2. Register\n" +
+                    "3. Exit");
+
+            // Takes input
+            int choice = reader.nextInt();
+            reader.nextLine();
+
+            switch (choice) {
+                case 1:
+                    login();
+                case 2:
+                    register();
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("Wrong choice, try again");
+            }
+        }
+    }
+
+    static void login() {
+        System.out.println("Please write your First name: ");
+
+    }
+
+    static void register() {}
 
     static void Menu() {
-        System.out.println(
-                "Choose an option:\n" +
-                "1. Add Book\n" +
-                "2. Add User\n" +
-                "3. Borrow Book\n" +
-                "4. Return Book\n" +
-                "5. Search Books\n" +
-                "6. Exit");
 
-        // Takes input
-        Scanner reader = new Scanner(System.in);
-        int choice = reader.nextInt();
-        //reader.close();
+        while (true) {
+            System.out.println(
+                    "Choose an option:\n" +
+                    "1. Add Book\n" +
+                    "2. Add User\n" +
+                    "3. Borrow Book\n" +
+                    "4. Return Book\n" +
+                    "5. Search Books\n" +
+                    "6. Exit");
 
-        // Methods for BookManager
-        BookManager manager = new BookManager();
+            // Takes input
+            int choice = reader.nextInt();
+            reader.nextLine();
 
-        // Variables
-        int id;
-
-        // Options
-        switch (choice) {
-            case 1: // Add book
-                System.out.print("Enter Book Title: ");
-                String title = reader.next();
-                System.out.print("Enter Book Author: ");
-                String author = reader.next();
-                System.out.print("Enter Book Year: ");
-                int year = reader.nextInt();
-                manager.addBook(title, author, year);
-                break;
-            case 2: // Add User
-                System.out.print("Enter user's First name: ");
-                String name = reader.next();
-                manager.addUser(name);
-                break;
-            case 3: // Borrow
-                manager.listAvailableBooks();
-                System.out.println("Please choose a book by writing its number:");
-                id = reader.nextInt();
-                manager.borrowBookID(id);
-                break;
-            case 4: // Return book
-                manager.listBorrowedBooks();
-                System.out.println("Please choose a book by writing its number:");
-                id = reader.nextInt();
-                manager.returnBookID(id);
-                break;
-            case 5:
-                manager.searchBook();
-                break;
-            case 6:
-                System.exit(0);
-            default:
-                System.out.println("Wrong choice, try again");
-                Menu();
+            // Options
+            switch (choice) {
+                case 1: // Add book
+                    System.out.print("Enter Book Title: ");
+                    String title = reader.nextLine();
+                    System.out.print("Enter Book Author: ");
+                    String author = reader.nextLine();
+                    System.out.print("Enter Book Year: ");
+                    int year = reader.nextInt();
+                    manager.addBook(title, author, year);
+                    break;
+                case 2: // Add User
+                    System.out.print("Enter user's First name: ");
+                    String name = reader.nextLine();
+                    manager.addUser(name);
+                    break;
+                case 3: // Borrow
+                    manager.listAvailableBooks();
+                    System.out.println("Please choose a book by writing its number:");
+                    int borrowId = reader.nextInt();
+                    reader.nextLine();
+                    manager.borrowBookID(borrowId);
+                    break;
+                case 4: // Return book
+                    manager.listBorrowedBooks();
+                    System.out.println("Please choose a book by writing its number:");
+                    int returnId = reader.nextInt();
+                    reader.nextLine();
+                    manager.returnBookID(returnId);
+                    break;
+                case 5:
+                    manager.searchBook(); //TODO pass reader if needed for taking input
+                    break;
+                case 6:
+                    reader.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Wrong choice, try again");
+            }
         }
     }
 
@@ -69,9 +102,7 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("Welcome to Library System!");
-        Menu();
 
-
+        welcome(); // Welcome screen
     }
 }
